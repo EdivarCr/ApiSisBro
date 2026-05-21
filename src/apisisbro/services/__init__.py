5b77d5be823113c7eaab12beb7b1baa071dcba06
EdivarCr <edivarfilho16@gmail.com>
2026-05-20 23:05:04 -0300

    feat(api): adiciona arquvis init.py para melhor acesso aos arquivos da pasta

---
+++ src/apisisbro/services/__init__.py
@@ -0,0 +1,11 @@
+from .entrada_insumo_service import EntradaInsumoService
+from .insumo_service import InsumoService
+from .product_service import ProductService
+from .production_service import ProductionService
+
+__all__ = [
+    'EntradaInsumoService',
+    'InsumoService',
+    'ProductionService',
+    'ProductService',
+]
